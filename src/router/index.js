5835c930a4747b21b9656c5d530c8a090741e349
baoqiang <baoqiang782@pingan.com.cn>
2021-01-14 18:44:32 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {HashRouter, Switch, Route} from 'react-router-dom'
 import Home from '../views/home'
 import GenerateDTS from '../views/generate-dts'
 
 const BasicRouter = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
           <Switch>
               <Route exact path='/' component={Home}></Route>
               <Route exact path='/generateDts' component={GenerateDTS}></Route>
           </Switch>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
